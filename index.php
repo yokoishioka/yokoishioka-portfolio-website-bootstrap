729a13ec4484c67abe04d6ad8aa05b319f61319f
yoko ishioka <yoko@yokoishioka.com>
2016-11-08 04:27:47 -0500

    added font links and header

--- index.php
+++ index.php
@@ -7,12 +7,27 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" />
   <link rel="stylesheet" href="css/style.css" />
   <script src="scripts/scripts.js"></script>
 </head>
 <body>
 
 <container class="container-fluid">
+<header class="row navbar-fixed-top">
+     <div class="col-md-4 pull-left">
+         <img class="thumbnail header" src="http://yokoishioka.com/wp-content/uploads/2016/11/yoko-ishioka-web-developer-graphic-designer-logo.png" alt="web developer and graphic designer logo - yoko ishioka">
+         <h2>Web Developer | Graphic Designer</h2>
+     </div>
+     <nav class="col-md-8 pull-right">
+         <a href="#nav-portfolio" title="portfolio">portfolio</a>
+         <a href="#nav-blog" title="blog">blog</a>
+         <a href="#nav-about" title="about">about</a>
+         <a href="#nav-contact" title="contact">contact</a>
+     </nav>
+  </header>
 </container>
 
 </body>
