7ae6c00a1aab0c582c6bdeccedb2031b60111191
yoko ishioka <yoko@yokoishioka.com>
2016-11-10 06:19:09 -0500

    redid the arrow and exit animations so they react the same way off of element with class called "remove-toggle-arrow"

--- scripts/index.js
+++ scripts/index.js
@@ -1,63 +1,31 @@
 $(document).ready(function() {
-   $('nav.nav').click(function() {
-      var navSpan = $(this).children('span');
-      if (navSpan.hasClass('glyphicon-menu-down')) {
-           $(this).css("color","white");
-           navSpan.removeClass('glyphicon-menu-down');
-           navSpan.addClass('glyphicon-menu-up');
-           $(this).siblings('article.collapse').slideDown();
-      }
-      else {
-           $(this).css("color", "#444444");
-           navSpan.removeClass('glyphicon-menu-up');
-           navSpan.addClass('glyphicon-menu-down');
-           $(this).siblings('article.collapse').slideUp();
-      }
+   $('.remove-toggle-arrow').each(function() {
+       $(this).append("<span class='glyphicon glyphicon-menu-down'></span>");
+
    });
-   $('nav.nav').mouseenter(function() {
+    $('.collapse').each(function() {
+        $(this).prepend("<span class='glyphicon glyphicon-remove'></span>");
+    });
+   $('.glyphicon-remove').mouseenter(function() {
+      $(this).parent().siblings('.remove-toggle-arrow').children('span').slideUp();
+      $(this).parent().siblings('.remove-toggle-arrow').children('span').slideDown();
+   });
+    $('.glyphicon-remove').click(function() {
+        $(this).parent().removeClass('in');
+        $(this).parent().siblings('.remove-toggle-arrow').children('span').removeClass('glyphicon-menu-up').addClass('glyphicon-menu-down');
+   });
+   $('.remove-toggle-arrow').mouseenter(function() {
       $(this).children('span').slideUp();
       $(this).children('span').slideDown(); 
    });
-   $('h3 a.link').each(function() {
-       $(this).append("<span class='glyphicon glyphicon-menu-down'></span>");
-   });
-
-   $('h3 a.link').click(function() {
-       if ($(this).hasClass("glyphicon-menu-down")) {
-           $(this).siblings("span.glyphicon-menu-down").remove();
-           $(this).append("<span class='glyphicon glyphicon-menu-up'></span>");
-       }
-       else if ($(this).hasClass("glyphicon-menu-up")) {
-           $(this).children("span-glyphicon-menu-up").remove();
-            $(this).append("<span class='glyphicon glyphicon-menu-down'></span>");
-       }
-   });
-   $('a.link').click(function() {
+   $('.remove-toggle-arrow').click(function() {
        if ($(this).children('span').hasClass('glyphicon-menu-down')) {
-           $(this).children('span').removeClass('glyphicon-menu-down').addClass('glyphicon-menu-up');
-           $(this).parent().next('div.collapse.in').slideUp();
+            $(this).children('span').removeClass('glyphicon-menu-down').addClass('glyphicon-menu-up');
+            $(this).siblings('collapse').addClass('in');
        }
        else {
            $(this).children('span').removeClass('glyphicon-menu-up').addClass('glyphicon-menu-down');
-           $(this).parent().next('div.collapse.in').slideDown();
+           $(this).siblings('collapse').removeClass('in');
        }
    });
-   $('.row.collapse').each(function() {
-      $(this).prepend("<span class='glyphicon glyphicon-remove'></span>");
-   });
-
-   $('.glyphicon-remove').mouseenter(function() {
-      $(this).css("color","#999999");
-      $(this).parent().siblings('nav.nav').children('span').slideUp();
-      $(this).parent().siblings('nav.nav').children('span').slideDown();
-   });
-   $('.glyphicon-remove').mouseleave(function() {
-      $(this).css("color", "#444444");
-   });
-   $('.glyphicon-remove').click(function() {
-      $(this).parent().slideUp();
-      $(this).parent().siblings('nav.nav').css("color","#444444");
-      $(this).parent().siblings('nav.nav').children('span').removeClass('glyphicon-menu-up').addClass('glyphicon-menu-down');
-   });
-
 });
