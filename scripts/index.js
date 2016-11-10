c803588272b912ffbc296150f45383f98b0c8d7d
yoko ishioka <yoko@yokoishioka.com>
2016-11-09 23:29:54 -0500

    added navigation and slideup animations

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,31 @@ $(document).ready(function() {
            $(this).css("color","white");
            navSpan.removeClass('glyphicon-menu-down');
            navSpan.addClass('glyphicon-menu-up');
-           $(this).siblings('article').slideDown();
+           $(this).siblings('article.collapse').slideDown();
       }
       else {
            $(this).css("color", "#444444");
            navSpan.removeClass('glyphicon-menu-up');
            navSpan.addClass('glyphicon-menu-down');
-           $(this).siblings('article').slideUp();
+           $(this).siblings('article.collapse').slideUp();
       }
    });
    $('nav.nav').mouseenter(function() {
       $(this).children('span').slideUp();
-      $(this).children('span').slideDown();
+      $(this).children('span').slideDown(); 
+   });
+   $('h3 a.link').each(function() {
+      $(this).append("<span class='glyphicon glyphicon-menu-down'></span>");
+   });
+   $('a.link').click(function() {
+       if ($(this).children('span').hasClass('glyphicon-menu-down')) {
+           $(this).children('span').removeClass('glyphicon-menu-down').addClass('glyphicon-menu-up');
+           $(this).parent().next('div.collapse.in').slideUp();
+       }
+       else {
+           $(this).children('span').removeClass('glyphicon-menu-up').addClass('glyphicon-menu-down');
+           $(this).parent().next('div.collapse.in').slideDown();
+       }
    });
    $('article.collapse').each(function() {
       $(this).prepend("<span class='glyphicon glyphicon-remove'></span>");
@@ -34,4 +47,5 @@ $(document).ready(function() {
       $(this).parent().siblings('nav.nav').css("color","#444444");
       $(this).parent().siblings('nav.nav').children('span').removeClass('glyphicon-menu-up').addClass('glyphicon-menu-down');
    });
+
 });
